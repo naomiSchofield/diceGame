9341f27f90a84a647c79b8856a22b765d06a1dbf
Naomi Schofield <naomi_s257@hotmail.com>
2020-05-14 10:42:59 +0100

    refactored and reduced use of gobal variables

--- script.js
+++ script.js
@@ -1,87 +1,53 @@
-//issues -> multiple dice pictures appear and 
-// the number on the dice doesn't gets replaced 
-// every time the number is pressed. 
-// need to figure out how to save the number when the button is pressed. 
-// Could push all the outcomes of the generateDiceNumber to an array...
-
-
+//issues -> can keep pressing the button after winning. 
 
 const rollDiceButton = document.getElementById("rollDice")
 const score = document.getElementById("score")
 const outcome = document.getElementById("winnerOrLoser")
 const diceImage = document.getElementById("diceImage")
-let text = document.createTextNode('')
 let displayTotal = document.getElementById('displayScore')
-let player1Score
-let scoreArray = []
-let totalScore
-displayTotal.appendChild(text)
-outcome.appendChild(text)
-score.appendChild(text)
-
-
+let player1Score = 0
 
 
 rollDiceButton.addEventListener("click", () => {
     //Why does this event listener work with an ID and not work with a class?
-    generateDiceNumber(1, 6)
+    let diceRoll2 = generateDiceNumber(1, 6)
+    generateImage(diceRoll2)
+    winLose(diceRoll2)
 })
 
 const generateDiceNumber = (min, max) => {
-
     min = Math.ceil(min)
     max = Math.floor(max)
     //Why do we need Math.floor(Math.random()*(max-min +1)) + min
-    player1Score = Math.floor(Math.random() * (max - min + 1)) + min
-    score.textContent = 'You rolled a ' + player1Score
-    score.classList.add('score')
-    console.log(player1Score)
-    scoreArray.push(player1Score)
-    console.log(scoreArray)
-    totalScore = scoreArray.reduce((a, b) => {
-        return a + b
-    }, 0)
-    console.log('score', totalScore) 
+    let diceRoll1 = Math.floor(Math.random() * (max - min + 1)) + min
+    player1Score += diceRoll1
+    score.textContent = 'You rolled a ' + diceRoll1
     
-    let dice = document.createElement("img")
-    dice.src = `./img/dice${player1Score}.png`
-    diceImage.appendChild(dice) 
+    return diceRoll1
+}
 
-    if (player1Score == 1) {
+const generateImage = (diceRoll3) => {
+    diceImage.src = `./img/dice${diceRoll3}.png`
+    console.log(`this is the score ${diceRoll3}`)
+}
+
+const winLose = (diceRoll4) => {
+
+    if (diceRoll4== 1) {
         outcome.textContent = 'You rolled a 1, you lose.'
         displayTotal.textContent = ''
+        player1Score = 0
+
     } 
-   else if (player1Score > 1 && totalScore < 20) {
+   else if (player1Score < 20) {
         outcome.textContent = 'Roll again'
-        displayTotal.textContent = 'Your total score is ' + totalScore +' you need 21 to win.'
+        displayTotal.textContent = 'Your total score is ' + player1Score +' you need 21 to win.'
     }
-   else if (totalScore > 20) {
+   else  {
         score.textContent = ''
         outcome.textContent = "Congratulations, you scored 21 or more, you win!"
         displayTotal.textContent = ''
+        player1Score = 0 
     }
-
+    
 }
-
-
-   
-
-
-
-
-
-
-
-
-
-// const addDiceRolls = () => {
-//     for (let i = 21; i > player1Score; )
-//     alert ('roll again')
-// }
-
-// addDiceRolls()
-
-//issues -> multiple dice pictures appear and 
-// the number on the dice doesn't gets replaced 
-// every time the number is pressed. 
-// need to figure out how to save the number when the button is pressed. 
